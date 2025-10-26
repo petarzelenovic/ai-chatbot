8753c570ffeadeeb218ff1a286c510defb17532f
Petar Zelenovic <petarzelenovic261@gmail.com>
2025-10-26 18:46:38 +0100

    adding mocked route

--- app/composables/useChat.ts
+++ app/composables/useChat.ts
@@ -15,11 +15,13 @@ export default function useChat() {
     };
   }
 
-  function sendMessage(message: string) {
+  async function sendMessage(message: string) {
     messages.value.push(createMessage(message, "user"));
-    setTimeout(() => {
-      messages.value.push(createMessage(`You said ${message}`, "assistant"));
-    }, 300);
+    const data = await $fetch<ChatMessage>("/api/ai", {
+      method: "POST",
+      body: { messages: messages.value },
+    });
+    messages.value.push(data);
   }
 
   return {
